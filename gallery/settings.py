19d609d3412539981cf6d2f6c530e0171a370125
jerushaotieno <jerushaotienocoding@gmail.com>
2022-03-27 06:27:20 +0300

    add navbar to the application

--- gallery/settings.py
+++ gallery/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'photogallery.apps.PhotogalleryConfig',
+    'photos',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
