5ef31e6e6f9ae67ef6994cbad4b8066361c123a9
jerushaotieno <jerushaotienocoding@gmail.com>
2022-03-28 02:31:53 +0300

    write tests for each of the location model methods

--- photogallery/tests.py
+++ photogallery/tests.py
@@ -3,6 +3,8 @@ from .models import Image, Category, Location
 
 # Create your tests here
 
+#  Image Model Tests
+
 class ImageTest(TestCase):
     '''
     testing image model
@@ -24,64 +26,103 @@ class ImageTest(TestCase):
 
     def tearDown(self):
         '''
-        test method to delete Image instances after each test is run
+        test method for deleting image instances after running each test
         '''
-        Category.objects.all().delete()
-        Location.objects.all().delete()
         Image.objects.all().delete()
-
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+        
     def test_instances(self):
         '''
-        test method to assert instances created during setUp
+        test method for asserting instances created during setUp
         '''
         self.assertTrue(isinstance(self.new_picture,Image))
-        self.assertTrue(isinstance(self.new_category, Category))
         self.assertTrue(isinstance(self.new_location, Location))
+        self.assertTrue(isinstance(self.new_category, Category))
 
     def test_save_image(self):
         '''
-        test method to ensure an Image instance has been correctly saved
+        test method for correctly saving an image instance 
         '''
         self.assertTrue(len(Image.objects.all()) == 2)
 
     def test_delete_image(self):
         '''
-        test method to ensure an Image instance has been correctly deleted
+        test method for correctly deleting an image instance
         '''
         self.new_picture.delete_image()
         self.assertTrue(len(Image.objects.all()) == 1)
 
     def test_update_image(self):
         '''
-        test method to ensure an Image instance has been correctly updated
+        test method for correctly updating an image instance
         '''
-        update_test = self.new_picture.update_image('images/updated.png')
-        self.assertEqual(update_test.image_link, 'images/updated.png')
+        update_test = self.new_picture.update_image(' ')
+        self.assertEqual(update_test.image_link, '')
 
     def test_get_all(self):
         '''
-        test method to ensure all instances of Image class have been retrieved
+        test method for retrieving all instances of the image class
         '''
         pictures = Image.get_all()
-        # print(pictures)
 
     def test_get_image_by_id(self):
         '''
-        test method to ensure Image instances can be retrieved by id
+        test method for retrieving image instances by id
         '''
         obtained_image = Image.get_image_by_id(self.another_picture.id)
-        # print(obtained_image.title)
 
     def test_search_image(self):
         '''
-        test method to ensure correct searching of an multiple image instances by category
+        test method for searching various image instances by category
         '''
         obtained_image = Image.search_image(self.new_picture.category)
-        # print(obtained_image) #todo: reference individual instances
 
     def test_filter_by_location(self):
         '''
-        test method to obtain image insatnces by location
+        test method for image instances by location
         '''
         obtained_image = Image.filter_by_location(self.another_picture.location)
         print(obtained_image)
+
+
+#  Location Model Tests
+
+class LocationTest(TestCase):
+    '''
+    test class for Locations model
+    '''
+    def setUp(self):
+        '''
+        test method for creating location instances before any tests are run
+        '''
+        self.new_location = Location(city='unknown city', country='unknown country')
+        self.new_location.save_location()
+
+    def test_save_location(self):
+        '''
+        test method for correctly saving a location instance
+        '''
+        self.assertTrue(len(Location.objects.all()) == 1)     
+
+    def test_delete_location(self):
+        '''
+        test method for deleting a location instance correctly 
+        '''
+        self.new_location.save_location()
+        self.new_location.delete_location()
+        self.assertTrue(len(Location.objects.all()) == 0)
+
+    def test_update_location(self):
+        '''
+        test method for updating a location instance correctly 
+        '''
+        update_locale = Location.update_location('unknown location', 'new location')
+        self.assertEqual(update_locale.city, 'new location')
+
+    def test_get_all(self):
+        '''
+        test method for retrieving all instances of the location class 
+        '''
+        locations = Location.get_all()
+        print(locations)
